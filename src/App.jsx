e520a5f7d5daaca9b783a98f37e30480b12c89da
MateoHenao06 <henaom001@gmail.com>
2025-03-06 13:27:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './layouts/Header/Header';
+import { Main } from './layouts/Main/Main'; 
+import { Footer } from './layouts/Footer/Footer';
+
+export function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
